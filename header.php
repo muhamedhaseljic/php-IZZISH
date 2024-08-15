fe8aa3a373cab8d408de1f33efc55bd8064cfda5
muhamed <muhamed.haseljic.20@size.ba>
2024-08-15 21:40:42 +0200

    vjezba sa bootstap temom

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Admin Dashboard</title>
+
+    <link rel="" href="styles.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/bootstrap4-retro.min.css">
+    
+    
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Shrikhand" rel="stylesheet">
+    
+    
+  </head>
+  <body>
+
+  <div class="bg-success navbar-dark text-white">
+    <div class="container">
+        
+      <nav class="navbar navbar-expand-lg navbar-dark  bg-success mb-5 ">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="inz_logo_-1.png" width="220" height="60" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+                <ul class="navbar-nav">
+                    
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                  <a href="index.php" class="text-white nav-link">Start</a>
+                  <a href="app.php" class="nav-link text-white">App example</a>
+                  <a href="form.php" class="nav-link text-white">Form example</a>
+                
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="register.php">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="login.php">Login</a>
+                    </li>
+                    
+                    
+               
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    </div>
+  </div>
+
+
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once "header.php";
+
+?>
+  
+
+<div class="jumbotron bg-secondary text-center text-danger mb-0 radius-0">
+  <div class="container">
+    <div class="ht-tm-header">
+      <h1 class="display-1 display-xxl text-uppercase">Retro</h1>
+      <span class="lead">This is a starter template with a jumbotron</span>
+
+      <div class="mt-4">
+        <a href="#!"
+           class="btn btn-danger btn-lg m-2 text-secondary">
+          <span>A Button</span>
+        </a>
+
+        <a href="#!"
+           target="_blank"
+           class="btn btn-danger text-secondary btn-lg m-2">
+          <span>Another Button</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron bg-secondary text-center text-danger mb-0 radius-0">
+  <div class="container">
+    <div class="ht-tm-header">
+      <h1 class="display-1 display-xxl text-uppercase">Retro</h1>
+      <span class="lead">This is a starter template with a jumbotron</span>
+
+      <div class="mt-4">
+        <a href="#!"
+           class="btn btn-danger btn-lg m-2 text-secondary">
+          <span>A Button</span>
+        </a>
+
+        <a href="#!"
+           target="_blank"
+           class="btn btn-danger text-secondary btn-lg m-2">
+          <span>Another Button</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container py-5">
+  <h1>Thanks</h1>
+
+  <p>Thank you for downloading this theme. If you have trouble or find a bug, please open an issue on GitHub:<br>
+    <a href="https://github.com/HackerThemes/theme-machine">https://github.com/HackerThemes/theme-machine</a></p>
+    <a href="#!" class="ht-tm-element btn btn-outline-primary">
+            <span class="fab fa-github mr-2"></span>
+            Git</a>
+  
+            <?php
+
+require_once "footer.php";
+
+?>
