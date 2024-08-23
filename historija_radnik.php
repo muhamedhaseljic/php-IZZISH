8e1ebfb739b14447b1ce28c445acacbddadc69ab
muhamed <muhamed.haseljic.20@size.ba>
2024-08-23 17:29:48 +0200

    dodavanje svih stranica za rad sa php

---
+++ historija_radnik.php
@@ -0,0 +1,234 @@
+
+<style>
+.custom-main-content {
+    margin-left: 0px; /* Space for the sidebar */
+    width: 100%;
+    padding: 100px;
+    padding-top:20px;
+    background-color: #16171b;
+    min-height: 100vh;
+    padding-bottom:0px;
+}
+
+.d-flex {
+    display: flex;
+}
+
+.justify-content-between {
+    justify-content: space-between;
+}
+
+.align-items-center {
+    align-items: center;
+}
+
+.mb-3 {
+    margin-bottom: 20px;
+}
+
+.custom-search-bar {
+    padding: 10px;
+    width: 300px;
+    border-radius: 20px;
+    border: none;
+    color: white;
+    background-color:#16171b;
+    border:1px solid white;
+}
+
+/* Table Styling */
+.custom-table {
+    
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 10px; /* Increased gap between rows */
+    
+}
+
+.custom-table th, .custom-table td {
+    padding: 15px;
+    text-align: left;
+    color: #fff;
+}
+
+thead tr {
+    background-color: #272c78;
+    
+
+}
+
+.custom-table thead th {
+    padding: 15px;
+    text-align: left;
+    color: #fff;
+    background-color: #272c78;
+    border: none; /* Remove header cell border */
+}
+
+.custom-table tbody tr td:first-child {
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+}
+
+.custom-table tbody tr td:last-child {
+    border-top-right-radius: 20px;
+    border-bottom-right-radius: 20px;
+}
+
+.custom-table thead th:first-child {
+    border-top-left-radius: 20px; /* Rounded left corner */
+    border-bottom-left-radius: 20px;
+}
+
+.custom-table thead th:last-child {
+    border-top-right-radius: 20px; /* Rounded right corner */
+    border-bottom-right-radius: 20px;
+}
+
+tbody tr:last-child {
+    border-bottom: none;
+}
+
+.custom-table tbody tr td {
+    border: none;
+    background-color: #212528;
+    vertical-align: middle;
+    
+}
+
+.custom-table tbody tr:hover td {
+    
+    background-color: #2b2f30;
+    
+    
+}
+
+.custom-profile-img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+}
+
+.custom-add-btn{
+    padding: 10px 20px;
+    color: #fff;
+    
+    background-color: #262c78;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    color: white;
+}
+.custom-add-btn:hover{
+    background-color: #484b8f;
+    color: #fff;
+    text-decoration:none;
+}
+.custom-edit-btn, .custom-delete-btn, .custom-view-btn {
+    background-color: #444;
+    border: none;
+    padding: 10px;
+    border-radius: 20px;
+    cursor: pointer;
+    color: white;
+}
+
+ .custom-edit-btn:hover, .custom-delete-btn:hover, .custom-view-btn:hover{
+    background-color: #555;
+}
+
+
+.custom-edit-btn {
+    margin-right: 10px;
+    background-color: #548ace;
+}
+.custom-edit-btn:hover{
+    background-color: #386bab;
+}
+
+.custom-delete-btn {
+    margin-right: 10px;
+    background-color: #ed484d;
+}
+.custom-delete-btn:hover{
+    background-color: #ba383c;
+}
+
+.custom-view-btn {
+    margin-right: 10px;
+    background-color: #807e7e;
+}
+.custom-view-btn:hover{
+    background-color: #666565;
+}
+
+button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+}
+.custom-main-content h1{
+    color:white;
+    margin-bottom:20px;
+}
+.scrolling-divv{
+    overflow-y: scroll;
+    height: 75vh;
+    box-sizing: border-box;
+    width: 100%;
+    scrollbar-width: thin;
+    scrollbar-color: white #16171b;
+    padding-right:5px;
+}
+
+    </style>
+    
+    <div class="custom-main-content">
+        <h1 >Historija radnika</h1>
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <input type="text" placeholder="Search name..." class="custom-search-bar">
+                
+            </div>
+            <div class="scrolling-divv">
+            <table class="table custom-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Ime i prezime</th>
+                        <th>Email</th>
+                        <th>Telefon</th>
+                        <th>Pozicija</th>
+                        <th>Plata</th>                        
+                        <th>Slika</th>
+                        <th>Datum rodjenja</th>
+                        <th>Akcije</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                <?php for($i=1;$i<10;$i++) : ?>
+                    <tr>
+                        <td><?php echo $i ?></td>
+                        <td>Medina Haseljić</td>
+                        <td>medina@gmail.com</td>
+                        <td>
+                            <?php
+                                $FourDigitRandomNumber = mt_rand(111111111,999999999);
+                                echo "+" . $FourDigitRandomNumber;
+                            ?>
+                        </td>
+                        <td>Radni Kmet</td>
+                        <td>$ 1350</td>
+                        <td><img src="Medina-Haseljic.jpg" alt="Edis" class="custom-profile-img"></td>
+                        <td>29. 10. 1999</td>
+                        <td>
+                            <button class="custom-view-btn"><span class="fas fa-eye"></span></button>
+                            
+                        </td>
+                    </tr>
+                    <?php endfor; ?>
+                    <!-- Repeat for other entries -->
+                </tbody>
+            </table>
+            </div>
+        </div>
