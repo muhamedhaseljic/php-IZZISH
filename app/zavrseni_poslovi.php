59015117b62964c988fd2b7d81322d1fe3f1e4b9
muhamedhaseljic <muhamed.haseljic.20@size.ba>
2024-12-24 19:45:53 +0100

    Jobs: popUp view

--- app/zavrseni_poslovi.php
+++ app/zavrseni_poslovi.php
@@ -172,6 +172,74 @@ tbody tr:last-child {
     padding-right:5px;
 }
 
+.kupac-details {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .kupac-details h2 {
+            margin-top: 0;
+        }
+
+        .kupac-details ul {
+            list-style-type: none;
+            padding: 0;
+            margin-top: 10px;
+        }
+
+        .kupac-details ul li {
+            margin-bottom: 8px;
+            font-size: 14px;
+        }
+
+.popup {
+            display: none;
+            position: fixed; 
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: rgba(0, 0, 0, 0.7); 
+            justify-content: center;
+            align-items: center;
+            z-index: 999;
+        }
+
+        .popup-content {
+            background-color: white;
+            margin: 15% auto; 
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%; 
+            margin-top:100px;
+            display: flex;
+            border-radius: 8px;
+            width: 500px;
+            text-align: left;
+            position: relative;
+            color:black;
+        }
+
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+            width: 20px;
+            height: 20px;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
     </style>
     
     <div class="custom-main-content content">
@@ -205,6 +273,7 @@ tbody tr:last-child {
             kupac.adress as customer_adress,
             kupac.service as service,
             kupac.city as customer_city,
+            kupac.objekat as customer_objekat,
             radnici.first_name as employee_name,
             radnici.last_name as employee_last_name,
 
@@ -235,7 +304,8 @@ tbody tr:last-child {
                         <td><?php echo $result['car_name'] ?></td>
                         <td><?php echo $result['price'] ?></td>
                         <td>
-                            <button class="custom-view-btn"><span class="fas fa-eye"></span></button>
+                        <button id="popupBtn" class="custom-view-btn view-customer-btn" data-customer='<?php echo json_encode($result); ?>'>
+                            <span class="fas fa-eye"></span></button>
                             
                         </td>
                     </tr>
@@ -244,6 +314,39 @@ tbody tr:last-child {
             </table>
             </div>
         </div>
+
+
+        <div id="popup" class="popup">
+    <div class="popup-content">
+        
+        <div class="kupac-details">
+            <h2 hidden  id="customer-id" > </h2>
+            <h2 hidden id="customer-first-name" > </h2>
+
+            <h2 hidden id="customer-last-name" > </h2>
+            <h2> <strong> Podaci o završenom poslu </strong></h2>
+            <p>---------------------------------------------------- </p>
+            <ul>
+                <li><strong>Email:</strong> <span id="customer-email"></span></li>
+                <li><strong>Telefon:</strong> <span id="customer-phone"></span></li>
+                <li><strong>Grad:</strong> <span id="customer-city"></span></li>
+                <li><strong>Adresa:</strong> <span id="customer-adress"></span></li>
+                <li><strong>Objekat:</strong> <span id="customer-objekt"></span></li>
+                <li><strong>Radnik zadužen za posao:</strong> <span id="customer-employee-name"></span> <span id="customer-employee-surname"></span></li>
+                <li><strong>Automobil: </strong> <span id="customer-car-name" ></span> <span id="customer-car-model" ></span></li>
+                <br>
+                <li><strong> <span id="customer-service" ></span></strong></li>
+                <li id="osobe-product-name"><strong>Osobe:</strong> <ul id="customer-product-name"></ul></li>
+                <div id="customer-product-food">
+                    <li><strong>Hrana:</strong> <span id="customer-product-food-name"></span></li>
+                    <li><strong>Tip:</strong> <span id="customer-product-food-type"></span></li>
+                    <li><strong>Opis:</strong> <span id="customer-product-food-description"></span></li>
+                </div>
+            </ul>
+        </div>
+        <span class="close" id="close-popup">&times;</span>
+    </div>
+</div>
 
         <script>
             document.getElementById('search-input').addEventListener('input', function() {
@@ -261,4 +364,71 @@ tbody tr:last-child {
             }
         });
     });
+
+    // popUp
+    document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.getElementById("popup");
+    const closeBtn = document.getElementById("close-popup");
+
+    function showPopup(customer) {
+
+        document.getElementById("osobe-product-name").style.display = "block";  // Show the person section
+        document.getElementById("customer-product-food").style.display = "block";
+
+        document.getElementById("customer-id").textContent = customer.jobs_id;
+        document.getElementById("customer-first-name").textContent = customer.employee_name;
+        document.getElementById("customer-last-name").textContent = customer.employee_last_name;
+        document.getElementById("customer-email").textContent = customer.customer_email;
+        document.getElementById("customer-phone").textContent = customer.customer_phone_number;
+        document.getElementById("customer-description").textContent = customer.description;
+        document.getElementById("customer-city").textContent = customer.customer_city;
+        document.getElementById("customer-adress").textContent = customer.customer_adress;
+        document.getElementById("customer-objekat").textContent = customer.customer_objekat;
+        document.getElementById("customer-employee-name").textContent = customer.employee_name;
+        document.getElementById("customer-employee-surname").textContent = customer.employee_last_name;
+        document.getElementById("customer-car-name").textContent = customer.car_name;
+        document.getElementById("customer-car-model").textContent = customer.car_model;
+        document.getElementById("customer-service").textContent = customer.service;
+
+        const produktOsobaNames = customer.produkt_osoba_names;
+        const productNameContainer = document.getElementById("customer-product-name");
+        productNameContainer.innerHTML = ''; 
+
+        if (produktOsobaNames) {
+            const productNamesArray = produktOsobaNames.split(',');
+            productNamesArray.forEach(function(name) {
+                const li = document.createElement('li');
+                li.textContent = name.trim(); 
+                productNameContainer.appendChild(li);
+            });
+            document.getElementById("customer-product-food").style.display = "none";
+        } else{
+            document.getElementById("osobe-product-name").style.display = "none";
+            document.getElementById("customer-product-food-name").textContent = customer.product_food_name;
+            document.getElementById("customer-product-food-type").textContent = customer.product_food_type;
+            document.getElementById("customer-product-food-description").textContent = customer.product_food_description;
+            
+            
+        }
+        
+        popup.style.display = "block";
+    }
+
+    document.querySelectorAll(".view-customer-btn").forEach(function(button) {
+        button.addEventListener("click", function() {
+            const employee = JSON.parse(this.getAttribute("data-customer"));
+            showPopup(employee);  
+        });
+    });
+
+    closeBtn.onclick = function() {
+        popup.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+});
         </script>
